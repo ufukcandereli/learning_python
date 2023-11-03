47fd5aae491a2c672559a3375f484359aa936af8
Ufuk Can Dereli <147918319+ufukcandereli@users.noreply.github.com>
2023-11-03 15:08:13 +0300

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,17 @@
+# Girilen sayılar içinden en büyüğünü bulan kod
+
+n = int(input("How many numbers will you enter: "))
+
+numbers = []
+
+for x in range(n):
+    number = int(input(f"Enter number {x + 1}: "))
+    numbers.append(number)
+
+number1 = numbers[0]
+for number in numbers:
+    if number > number1:
+        number1 = number
+
+print(f"The largest number is: {number1}")
+
