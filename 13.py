d8fd5dc1f0b09a97d03484a7fdd12c6b329e63a1
Ufuk Can Dereli <147918319+ufukcandereli@users.noreply.github.com>
2023-11-03 14:40:08 +0300

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,15 @@
+# for loop ile boyları bilinen kişilerin boy ortalamasını alma
+
+n = int(input("How many values will you enter?: "))
+
+height_list = []
+
+total_height = 0
+for i in range(n):
+    height = float(input(f"{i + 1}. person's height (metre type): "))
+    height_list.append(height)
+    total_height += height
+
+avarage_height = total_height / n
+
+print(f"Avarage height is {round(avarage_height, 3)}")
