95b201a433d89213031d19a106bc41de75e1cb14
Ufuk Can Dereli <147918319+ufukcandereli@users.noreply.github.com>
2023-11-03 15:32:07 +0300

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,10 @@
+# Seçilen sayıya kadar olan tüm çift sayıların toplamı
+
+print("Enter a number and ı will sum all the even numbers from 0 to the one you enter: ")
+the_one = int(input())
+
+even_sum = 0
+
+for x in range(0, the_one + 1, 2):   # range'deki ikinci değer açık aralık sayılıyomuş, +1 ekledim
+    even_sum += x
+print(f"Total number is {even_sum}")
